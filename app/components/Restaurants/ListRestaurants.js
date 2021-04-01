2abaeb732843ea7eeac67382693419a84552ed7a
Jean Osco <willjean29@gmail.com>
2021-04-01 18:02:57 -0500

    list restaurants

---
+++ app/components/Restaurants/ListRestaurants.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import { View, StyleSheet, FlatList, Text, ActivityIndicator, TouchableOpacity} from 'react-native';
+import { Image } from 'react-native-elements';
+import ImageDefault from '../../../assets/img/no-image.png';
+const ListRestaurants = ({restaurants,handleLoadMore,isLoading}) => {
+  const restaurantes = [];
+  return (  
+    <View style={styles.viewRestaurants}>
+
+      {
+        (restaurants.length > 0 ) ? (
+          <>
+            <FlatList
+              data={restaurants}
+              renderItem={({item}) => <RestaurantItem restaurant={item}/>}
+              keyExtractor={(item) => item.id}
+              onEndReachedThreshold={0.5}
+              onEndReached={handleLoadMore}
+              ListFooterComponent={<FooterList isLoading={isLoading}/>}
+            />
+          </>
+        ) : (
+          <View style={styles.loaderRestaurants}>
+            <ActivityIndicator size="large" color="#00a680"/>
+            <Text>Cargando Restaurantes ... </Text>
+          </View>
+        )
+      }
+    </View>
+  );
+}
+
+const RestaurantItem = ({restaurant}) => {
+  console.log(restaurant);
+  const goRestaurant = () => {
+    console.log("OK");
+  }
+  return (
+    <TouchableOpacity onPress={goRestaurant}>
+      <View style={styles.viewRestaurant}>
+        <View style={styles.viewRestaurantImage}>
+          <Image
+            resizeMode="cover"
+            source={
+              restaurant.images[0] ? (
+                {uri : restaurant.images[0]}
+              ) : (
+                ImageDefault
+              )
+            }
+            PlaceholderContent={<ActivityIndicator size="large" color="#00a680"/>}
+            style={styles.imageRestaurant}
+          />
+        </View>
+        <View>
+          <Text style={styles.restaurantName}>
+            {restaurant.name}
+          </Text>
+          <Text style={styles.restaurantAddress}>
+            {restaurant.address}
+          </Text>
+          <Text style={styles.restaurantDescription}>
+            {restaurant.description.substr(0,60)}...
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const FooterList = ({isLoading}) => {
+  if(isLoading){
+    return (
+      <View style={styles.loaderRestaurants}>
+        <ActivityIndicator size="large" color="#00a680"/>
+      </View>
+    )
+  }else{
+    return (
+      <View>
+        <Text>No hay mas restaurantes que cargar</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  viewRestaurants: {
+    // flex: 1,
+    // justifyContent: "center"
+  },  
+  viewRestaurant: {
+    flexDirection: "row",
+    margin: 10
+  },
+  loaderRestaurants: {
+    alignItems: "center"
+  },
+  viewRestaurantImage:{
+    marginRight: 15,
+  },
+  imageRestaurant: {
+    width: 80,
+    height: 80
+  },
+  restaurantName: {
+    fontWeight: "bold"
+  },
+  restaurantAddress: {
+    paddingTop: 5,
+    color: "gray",
+  },
+  restaurantDescription: {
+    paddingTop: 2,
+    color: "gray",
+    width: 250
+  }
+})
+ 
+export default ListRestaurants;
--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -2,21 +2,68 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
+import ListRestaurants from '../../components/Restaurants/ListRestaurants';
 import AuthContext from '../../context/auth/AuthContext';
-
+import firebase from '../../database/firebase';
 const Restaurants = () => {
   const {user, reloadUser} = useContext(AuthContext); 
   const navigation = useNavigation();
   const [reloadUserInfo, setReloadUserInfo] = useState(false);
+  const [restaurants, setRestaurants] = useState([]);
+  const [totalRestaurants, setTotalRestaurants] = useState(0);
+  const [startRestaurants, setStartRestaurants] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const limitRestaurants= 7;
+
+  useEffect(() => {
+    const getRestaurants = async() => {
+      const response = await firebase.db.collection("restaurants").get();
+      setTotalRestaurants(response.size);
+      const resultRestaurants = [];
+      const restaurants = await firebase.db.collection("restaurants").orderBy("createAt", "desc").limit(limitRestaurants).get();
+      setStartRestaurants(restaurants.docs[restaurants.docs.length - 1]);
+      restaurants.forEach((doc) => {
+        const restaurant = doc.data();
+        restaurant.id = doc.id;
+        resultRestaurants.push(restaurant);
+      });
+      setRestaurants(resultRestaurants);
+    }
+    getRestaurants();
+  }, [])
   useEffect(() => {
     reloadUser();
     setReloadUserInfo(false);
   }, [reloadUserInfo])
+
+  const handleLoadMore = async() => {
+    const resultRestaurants = [];
+    restaurants.length < totalRestaurants && setIsLoading(true);
+    const newRestaurants = await firebase.db.collection("restaurants").orderBy("createAt", "desc").startAfter(startRestaurants.data().createAt).limit(limitRestaurants).get();
+    if(newRestaurants.docs.length > 0){
+      setStartRestaurants(newRestaurants.docs[newRestaurants.docs.length - 1])
+    }else{
+      setIsLoading(false);
+    }
+
+    newRestaurants.forEach((doc) => {
+      const restaurant = doc.data();
+      restaurant.id = doc.id;
+      resultRestaurants.push(restaurant);
+    });
+
+    setRestaurants([...restaurants, ...resultRestaurants]);
+  }
   return ( 
     <View
       style={styles.viewBody}
     >
-      <Text>Restaurants</Text>
+      {/* <Text>Restaurants</Text> */}
+      <ListRestaurants
+        restaurants={restaurants}
+        handleLoadMore={handleLoadMore}
+        isLoading={isLoading}
+      />
       {
         user && (
           <Icon
